8d78193f142fa3ede77ce6b0eeab82df5c303d90
Vladimir Boyko <vboyko024@gmail.com>
2018-09-30 17:14:03 +0700

    Now open login page if no token

--- chat-app/src/components/PrivateRoute.js
+++ chat-app/src/components/PrivateRoute.js
@@ -3,12 +3,12 @@ import { Route, Redirect } from 'react-router-dom';
 import { hasToken } from '../helpers/token';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={ props => (
-        hasToken
-        ? <Component {...props} />
-        : <Redirect to={{
-            pathname: '/login',
-            state: { from: props.location }
-          }} />
+    <Route {...rest} render={props => (
+        hasToken()
+            ? <Component {...props} />
+            : <Redirect to={{
+                pathname: '/login',
+                state: { from: props.location }
+            }} />
     )} />
-  )
+)
