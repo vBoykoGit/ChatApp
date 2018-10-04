53d9e7cc75be2fe48ed670805436dd8aa560de55
Vladimir Boyko <vboyko024@gmail.com>
2018-10-04 12:27:19 +0700

    Styles for chatInfo fixed + common colors created

--- chat-app/src/components/ChatInfo.js
+++ chat-app/src/components/ChatInfo.js
@@ -3,4 +3,4 @@ import React from "react";
 export const ChatInfo = ({ title = 'Channel title' }) =>
     <div>
         <p> {title} </p>
-    </div>
+    </div >
--- chat-app/src/components/containers/Chats.js
+++ chat-app/src/components/containers/Chats.js
@@ -9,8 +9,10 @@ import { NavLink } from 'react-router-dom'
 const Chats = ({ channels = [] }) =>
   <div className="chats">
     {channels.map(channel =>
-      <NavLink key={channel._id} exact to={`/channel/${channel._id}`} className="chats__chatInfo" activeClassName="chats__chatInfo_activeStyle">
-        <ChatInfo title={channel.title === null || channel.title === '' || channel.title === undefined ? channel.name : channel.title} />
+      <NavLink key={channel._id} exact to={`/channel/${channel._id}`} className="chats__chatInfo" activeClassName="chats__chatInfo chats__chatInfo_activeStyle">
+        <span >
+          <ChatInfo title={channel.title === null || channel.title === '' || channel.title === undefined ? channel.name : channel.title} />
+        </span>
       </NavLink>)}
   </div>
 
